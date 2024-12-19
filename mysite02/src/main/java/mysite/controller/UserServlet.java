1a4dd4268e848d9f4cb659244e41a9e822d5e2bd
h7eona <jessie117@naver.com>
2024-12-19 22:05:37 +0900

    guestbook, user, update 과제 제출

---
+++ mysite02/src/main/java/mysite/controller/UserServlet.java
@@ -0,0 +1,35 @@
+package mysite.controller;
+
+import java.util.Map;
+
+import jakarta.servlet.annotation.WebServlet;
+import mysite.controller.action.main.MainAction;
+import mysite.controller.action.user.JoinAction;
+import mysite.controller.action.user.JoinFormAction;
+import mysite.controller.action.user.JoinSuccessAction;
+import mysite.controller.action.user.LoginAction;
+import mysite.controller.action.user.LoginFormAction;
+import mysite.controller.action.user.LogoutAction;
+import mysite.controller.action.user.UpdateAction;
+import mysite.controller.action.user.UpdateFormAction;
+
+@WebServlet("/user")
+public class UserServlet extends ActionServlet {
+	private static final long serialVersionUID = 1L;
+
+	private Map<String, Action> mapAction = Map.of(
+		"joinform", new JoinFormAction(),
+		"join", new JoinAction(),
+		"joinsuccess", new JoinSuccessAction(),
+		"loginform", new LoginFormAction(),
+		"login", new LoginAction(),
+		"logout", new LogoutAction(),
+		"updateform", new UpdateFormAction(),
+		"update", new UpdateAction()
+	);
+	
+	@Override
+	protected Action getAction(String actionName) {
+		return mapAction.getOrDefault(actionName, new MainAction());
+	}
+}
