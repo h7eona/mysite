2fc4a23044639e5c7017b379f1552ffb4e34161f
h7eona <jessie117@naver.com>
2025-01-17 16:01:29 +0900

    Spring Security 적용 #finish

--- mysite05/src/main/java/mysite/config/app/SecurityConfig.java
+++ mysite05/src/main/java/mysite/config/app/SecurityConfig.java
@@ -1,10 +1,10 @@
 package mysite.config.app;
 
-
 import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
@@ -24,71 +25,89 @@ import jakarta.servlet.http.HttpServletResponse;
 import mysite.repository.UserRepository;
 import mysite.security.UserDetailsServiceImpl;
 
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
-	
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    	http
-    		.csrf((csrf) -> {
-    			csrf.disable();
-    		})
-    		.formLogin((formLogin) -> {
-    			formLogin
-    				.loginPage("/user/login")
-    				.loginProcessingUrl("/user/auth")
-    				.usernameParameter("email")
-    				.passwordParameter("password")
-    				.defaultSuccessUrl("/")
-    				//.failureUrl("/user/login?result=fail");
-    				.failureHandler(new AuthenticationFailureHandler() {
+        http
+        	.csrf(csrf -> csrf.disable())
+        	.formLogin(formLogin -> {
+        		formLogin
+        			.loginPage("/user/login")
+        			.loginProcessingUrl("/user/auth")
+        			.usernameParameter("email")
+        			.passwordParameter("password")
+        			.defaultSuccessUrl("/")
+        			// .failureUrl("/user/login?result=fail");
+        			.failureHandler(new AuthenticationFailureHandler() {
 						@Override
 						public void onAuthenticationFailure(
-							HttpServletRequest request, 
+							HttpServletRequest request,
 							HttpServletResponse response,
 							AuthenticationException exception) throws IOException, ServletException {
 							request.setAttribute("email", request.getParameter("email"));
 							request.setAttribute("result", "fail");
-							
 							request
 								.getRequestDispatcher("/user/login")
 								.forward(request, response);
 						}
-    					
-    				});
-    		})
-    		.authorizeHttpRequests((authorizeRequests) -> {
-    			/* ACL */
-    			authorizeRequests
+        			});
+        	})
+        	.logout(logout -> {
+        		logout
+        			.logoutUrl("/user/logout")
+        			.logoutSuccessUrl("/");
+        	})
+        	.authorizeHttpRequests(authorizeRequests -> {
+        		/* ACL */
+        		authorizeRequests
+    				.requestMatchers(new RegexRequestMatcher("^/admin/?.*$", null))
+    				.hasAnyRole("ADMIN")
+
     				.requestMatchers(new RegexRequestMatcher("^/user/update$", null))
-    				.authenticated()
-    				
-    				.anyRequest()
-    				.permitAll();
-    		});
-        return http.build();
+        			//.authenticated()
+        			.hasAnyRole("ADMIN", "USER")
+
+    				.requestMatchers(new RegexRequestMatcher("^/board/?(write|delete|view)$", null))
+        			.hasAnyRole("ADMIN", "USER")
+        			
+        			.anyRequest()
+        			.permitAll();
+        	})
+        	.exceptionHandling(exceptionHandling -> {
+//        		 exceptionHandling.accessDeniedPage("/WEB-INF/views/errors/403.jsp");
+        		exceptionHandling.accessDeniedHandler(new AccessDeniedHandler() {
+					@Override
+					public void handle(
+						HttpServletRequest request,
+						HttpServletResponse response,
+						AccessDeniedException accessDeniedException) throws IOException, ServletException {
+						response.sendRedirect(request.getContextPath());
+					}
+        		});
+        	});
+        
+    	return http.build();
     }
     
     @Bean
     public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncode) {
     	DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
     	
-    	authenticationProvider.setPasswordEncoder(passwordEncode);
     	authenticationProvider.setUserDetailsService(userDetailsService);
+    	authenticationProvider.setPasswordEncoder(passwordEncode);
     	
     	return new ProviderManager(authenticationProvider);
     }
     
     @Bean
     public PasswordEncoder passwordEncoder() {
-    	return new BCryptPasswordEncoder(4); /* 4 ~ 31 */
+    	return new BCryptPasswordEncoder(4); // 4 ~ 31 
     }
     
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
     	return new UserDetailsServiceImpl(userRepository);
     }
-
 }
--- mysite05/src/main/java/mysite/controller/UserController.java
+++ mysite05/src/main/java/mysite/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.validation.Valid;
-import mysite.security.Auth;
 import mysite.service.UserService;
 import mysite.vo.UserVo;
 
@@ -52,8 +51,7 @@ public class UserController {
 	public String login() {
 		return "user/login";
 	}
-	
-	@Auth
+
 	@RequestMapping(value="/update", method=RequestMethod.GET)
 	public String update(/*HttpSession session,*/Authentication authentication, Model model) {
 //		1. HttpSession 을 사용하는 방법
@@ -73,7 +71,6 @@ public class UserController {
 		return "user/update";
 	}
 
-	@Auth
 	@RequestMapping(value="/update", method=RequestMethod.POST)
 	public String update(Authentication authentication, UserVo userVo) {
 		UserVo authUser = (UserVo) authentication.getPrincipal();
